18b6eb00302e24f0b90399247710c0bdabe321dd
Francis Allan Barcelona <tet@klab.com>
2013-08-15 11:12:32 +0800

    revisions/fixed-MobileArrayAdapter

--- src/com/klabcyscorpions/exercises/ListMobileActivity.java
+++ src/com/klabcyscorpions/exercises/ListMobileActivity.java
@@ -1,22 +1,35 @@
 package com.klabcyscorpions.exercises;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
  
 public class ListMobileActivity extends ListActivity {
- 
-	static final String[] MOBILE_OS = 
-               new String[] { "Android", "iOS", "WindowsMobile", "Blackberry"};
- 
+ Context context;
+	static ArrayList<HashMap<String, String>> MOBILE_OS = new ArrayList<HashMap<String, String>>();
+	String mobiles[] = {"WindowsMobile","iOS", "Blackberry", "Android" };
+     int pic[] = {R.drawable.android_logo,R.drawable.blackberry_logo,R.drawable.ios_logo,R.drawable.windowsmobile_logo};        
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
- 
+		
+		int mobileCount = mobiles.length;
+		for(int i = 0; i < mobileCount; i++){
+			HashMap<String, String> mobile = new HashMap<String, String>();
+			
+			mobile.put("name", mobiles[i]);
+			mobile.put("pic", String.valueOf(pic[i]));
+			MOBILE_OS.add(mobile);
+		}
 		setListAdapter(new MobileArrayAdapter(this, MOBILE_OS));
- 
 	}
  
 	@Override
--- src/com/klabcyscorpions/exercises/MobileArrayAdapter.java
+++ src/com/klabcyscorpions/exercises/MobileArrayAdapter.java
@@ -1,7 +1,6 @@
 package com.klabcyscorpions.exercises;
 
-
-	import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.klabcyscorpions.exercises.ListViewAdapter.ContactsViewHolder;
@@ -11,53 +10,91 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-	 
-	public class MobileArrayAdapter extends ArrayAdapter<String> {
-		private final Context context;
-		private final String[] values;		
-		
-		static class mobileViewHolder {
-	        TextView mobileText;
-	        ImageView mobileImage;
-	    }
-		public MobileArrayAdapter(Context context, String[] values) {
-			super(context, R.layout.list_mobile, values);
-			this.context = context;
-			this.values = values;
-		}
-	 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			mobileViewHolder viewHolder;
-			
-			if(convertView == null){
-			LayoutInflater inflater = (LayoutInflater) context
+
+public class MobileArrayAdapter extends ArrayAdapter<String> {
+	private final Context context;
+	ArrayList<HashMap<String, String>> mobiles;
+	ImageLoader imageLoader;
+	
+	static class mobileViewHolder {
+		TextView mobileText;
+		ImageView mobileImage;
+	}
+
+	public MobileArrayAdapter(Context context,
+            ArrayList<HashMap<String, String>> mobiles) {
+		super(context, R.layout.list_mobile);
+		this.context = context;
+		this.mobiles = mobiles;
+	}
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return mobiles.size();
+	}
+
+	@Override
+	public String getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		mobileViewHolder viewHolder;
+		viewHolder = new mobileViewHolder();
+		LayoutInflater inflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			
+		convertView = inflater.inflate(R.layout.list_mobile, parent, false);
+		
+		HashMap<String, String> mobile = mobiles.get(position);
+		((TextView) convertView.findViewById(R.id.label)).setText(mobile.get("name"));
+		((ImageView) convertView.findViewById(R.id.logo)).setImageResource(Integer.valueOf(mobile.get("pic")));
+		
+		return convertView;
+		/*if (convertView == null) {
+			LayoutInflater inflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
 			convertView = inflater.inflate(R.layout.list_mobile, parent, false);
 			viewHolder = new mobileViewHolder();
-			viewHolder.mobileText = (TextView) convertView.findViewById(R.id.label);
-			viewHolder.mobileImage = (ImageView) convertView.findViewById(R.id.logo);	
-			}
-			else {
-	            viewHolder = (mobileViewHolder) convertView.getTag();
-	        }
-			String s = values[position];
-			viewHolder.mobileText.setText(s);
-			if(s != null) {
-				if (s.equals("WindowsMobile")) {
-					viewHolder.mobileImage.setImageResource(R.drawable.windowsmobile_logo);
-				} else if (s.equals("iOS")) {
-					viewHolder.mobileImage.setImageResource(R.drawable.ios_logo);
-				} else if (s.equals("Blackberry")) {
-					viewHolder.mobileImage.setImageResource(R.drawable.blackberry_logo);
-				} else {
-					viewHolder.mobileImage.setImageResource(R.drawable.android_logo);
-				}
-			}
-			return convertView;
+			viewHolder.mobileText = (TextView) convertView
+					.findViewById(R.id.label);
+			viewHolder.mobileImage = (ImageView) convertView
+					.findViewById(R.id.logo);
+			
+			 convertView.setTag(viewHolder);
+		} else {
+			viewHolder = (mobileViewHolder) convertView.getTag();
 		}
-	}
+		HashMap<String, String> s = mobiles.get(position);
+		viewHolder.mobileText.setText((CharSequence) s);
+		if (s != null) {
+			if (s.equals("WindowsMobile")) {
+				viewHolder.mobileImage
+						.setImageResource(R.drawable.windowsmobile_logo);
+			} else if (s.equals("iOS")) {
+				viewHolder.mobileImage.setImageResource(R.drawable.ios_logo);
+			} else if (s.equals("Blackberry")) {
+				viewHolder.mobileImage
+						.setImageResource(R.drawable.blackberry_logo);
+			} else {
+				viewHolder.mobileImage
+						.setImageResource(R.drawable.android_logo);
+			}
+		}
+		return convertView;
+	*/
+		}
+}
 	
